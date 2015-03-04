0526f736636dfc4e906a6924202b62a289724968
Kyle Giard-Chase & Bryan Borgeson <kgiardch@gmail.com>
2015-03-03 16:27:24 -0800

    Create get search_result method. Instantiate cars within car class. Create array. Create for loop to get price and mile user input. Create empty string to store each iteration of the foreach loop. Return .

--- car_dealership/app/app.php
+++ car_dealership/app/app.php
@@ -14,13 +14,13 @@
         <!DOCTYPE html>
         <html>
         <head>
-            <link rel='stylesheeet' href='hhttps://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'/>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
             <title>Find a Car</title>
         </head>
         <body>
             <div class='container'>
                 <h1>Find a Car!</h1>
-                <form action='car.php'>
+                <form action='/search_result'>
                     <div class='form-group'>
                         <label for='price'>Enter Maximum Price:</label>
                         <input id='price' name='price' class='form-control' type='number'/>
@@ -39,8 +39,51 @@
     });
 
     $app->get("search_result", function() {
+        $porsche = new Car("2014 Porsche 911", 114991, 7684);
+        $ford = new Car("2011 Ford F450", 55995, 14241);
+        $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+        $mercedes = new Car("Mercedes Benz CLS550", 399000, 37979);
+
+        $cars = array($porsche, $ford, $lexus, $mercedes);
+
+        $cars_matching_search = array ();
+            foreach($cars as $car) {
+                if($car->worthBuying($_GET['price']) && ($car->worthMileage($_GET['miles']))) {
+                    array_push($cars_matching_search, $car);
+                }
+            }
+
+        if (empty($cars_matching_search)) {
+            return 'There are no cars that meet your search criteria.  Please try again.';
+        } else {
+            $output = "";
+            foreach ($cars_matching_search as $car) {
+                $output = $output . "
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+                    <title>Find a Car</title>
+                </head>
+                <body>
+                    <div class='container'>
+                        <h2>" . $car->getMakeModel() . "</h2>
+                        <ul>
+                            <li>Price: " . $car->getPrice() . "</li>
+                            <li>Mileage: " . $car->getMiles() . "</li>
+                        </ul>
+                    </div>
+                </body>
+                </html>
+                ";
+            }
+
+            return $output;
+            
+        }
 
     });
+
     return $app;
 
 ?>
